c4cc079076ee1b8a2eb28306c6078e2095396f77
aricalves <aric.alves2012@gmail.com>
2017-10-10 15:59:11 -0700

    Finish required problems

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+const stringifyJSON = function(obj) {
+  
+  let stringified;
+
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+    stringified = String(obj);
+  } else if (typeof obj === 'string') {
+    stringified = '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    if (!obj.length) {
+      stringified = '[]';
+    } else {
+      stringified = '[' + obj.map(el => stringifyJSON(el)) + ']';
+    }
+  } else if (obj === undefined || typeof obj === 'function') {
+    return;
+  } else {
+    if (!Object.keys(obj).length) {
+      stringified = '{}';
+    } else {
+      let objectMeat = [];
+      for (let key in obj) { 
+        if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+          objectMeat.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key])); 
+        } 
+      }
+      stringified = '{' + objectMeat.join(',') + '}';
+    }
+  }
+  return stringified;
 };
