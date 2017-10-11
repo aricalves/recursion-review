ae63c18ffc4b24838d7f0f3922a88ba11fb8d5c3
aricalves <aric.alves2012@gmail.com>
2017-10-10 19:56:41 -0700

    Refactor getElementsByClassName to not use an inner function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,20 +4,59 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-  var resultNodes = [];
 
-  var searchNodes = function(nodes) {
-    for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i].classList && nodes[i].classList.contains(className)) {
-        resultNodes.push(nodes[i]);
-      }
-      if (nodes[i].childNodes) {
-        searchNodes(nodes[i].childNodes);
-      }
-    }
-  };
+// var getElementsByClassName = function(className) {
+//   var resultNodes = [];
+
+//   var searchNodes = function(nodes) {
+//     for (let i = 0; i < nodes.length; i++) {
+//       if (nodes[i].classList && nodes[i].classList.contains(className)) {
+//         resultNodes.push(nodes[i]);
+//       }
+//       if (nodes[i].childNodes) {
+//         searchNodes(nodes[i].childNodes);
+//       }
+//     }
+//   };
   
-  searchNodes(document.childNodes);
-  return resultNodes;
+//   searchNodes(document.childNodes);
+//   return resultNodes;
+// };
+
+var getElementsByClassName = function(className, nodes, results) {
+  
+  results = results || [];
+  
+  nodes = nodes || document.childNodes;
+  
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].classList && nodes[i].classList.contains(className)) {
+      results.push(nodes[i]);
+    }
+    if (nodes[i].childNodes) {
+      getElementsByClassName(className, nodes[i].childNodes, results);
+    }
+  }
+  
+  return results;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
